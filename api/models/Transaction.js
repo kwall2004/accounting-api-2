c706fbaf29dac2f4eccc149910c151d28bae9bf6
Kevin Wall <kwall2004@gmail.com>
2018-12-27 13:45:17 -0500

    Connect to MySQL.

--- api/models/Transaction.js
+++ api/models/Transaction.js
@@ -9,21 +9,28 @@ module.exports = {
 
   attributes: {
 
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    id: {
+      type: 'number',
+      required: true,
+      columnName: 'transId'
+    },
+    date: {
+      type: 'ref',
+      required: true,
+      columnName: 'transDate',
+      columnType: 'datetime'
+    },
+    description: {
+      type: 'string',
+      columnName: 'transDescription'
+    },
+    amount: {
+      type: 'number',
+      columnName: 'transAmount'
+    },
+    createdAt: false,
+    updatedAt: false
 
   },
 
 };
-
