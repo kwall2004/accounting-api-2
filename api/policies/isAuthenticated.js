4349a5eb1c2a8664365ae1383e79a1c23a054db6
Kevin Wall <kwall2004@gmail.com>
2018-12-28 15:53:13 -0500

    Add auth.

---
+++ api/policies/isAuthenticated.js
@@ -0,0 +1,8 @@
+import * as jwt from 'express-jwt';
+
+var authCheck = jwt({
+  secret: new Buffer(sails.config.auth.clientSecret, 'base64'),
+  audience: sails.config.auth.clientId
+});
+
+module.exports = authCheck;
