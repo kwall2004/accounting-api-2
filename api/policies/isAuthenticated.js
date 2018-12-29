b692bbc84f3373050c5acee6883a7524a7f15f2b
Kevin Wall <kwall2004@gmail.com>
2018-12-28 23:05:18 -0500

    Specify RS256 algorithm.

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -2,7 +2,8 @@ var jwt = require('express-jwt');
 
 var authCheck = jwt({
   secret: new Buffer(sails.config.auth.clientSecret, 'base64'),
-  audience: sails.config.auth.clientId
+  audience: sails.config.auth.clientId,
+  algorithms: ['RS256']
 });
 
 module.exports = authCheck;
