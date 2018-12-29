bfc74433b82cccf8807f4d9be8a4f9bc95fea26a
Kevin Wall <kwall2004@gmail.com>
2018-12-29 00:49:57 -0500

    Return 401 response.

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -11,6 +11,12 @@ var authCheck = jwt({
   audience: 'https://accounting-api/',
   issuer: 'https://kwall2004.auth0.com/',
   algorithms: ['RS256']
+}, function (err, req, res, next) {
+  if (!err) {
+    return next;
+  }
+
+  return res.status(401).json(err);
 });
 
 module.exports = authCheck;
